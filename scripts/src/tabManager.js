99720e2177e5a9265402b9c69072cd1acea91a4e
adamjamesadair <adam.james.adair@gmail.com>
2018-05-22 10:44:52 +0900

    Refactored

--- scripts/src/tabManager.js
+++ scripts/src/tabManager.js
@@ -192,28 +192,6 @@ function arrangeWindowTabs(win) {
   }
 }
 
-/*
- * Converts the string of a url to URL type.
- *
- * @param {string} str
- *   The string to convert.
- *
- * @param {bool} hostname
- *   true to remove the 'www.' from the url's hostname. Default: true.
- *
- * @return {URL} url
- *  The converted URL.
- */
-function strToURL(str, hostname = true) {
-  let url = new URL(str);
-  if (hostname) {
-    if (url.hostname.includes('.')) {
-      url.hostname = url.hostname.replace(/^www\./, '');
-    }
-  }
-  return (url);
-}
-
 function initSettings(tabManager, settings) {
   let searchScope = settings['searchScope'] ? settings['searchScope'] : "both";
   let sortMethod = settings['sortMethod'] ? settings['sortMethod'] : "alphabetically";
---
+++ scripts/utils/utils.js
@@ -0,0 +1,21 @@
+/*
+ * Converts the string of a url to URL type.
+ *
+ * @param {string} str
+ *   The string to convert.
+ *
+ * @param {bool} hostname
+ *   true to remove the 'www.' from the url's hostname. Default: true.
+ *
+ * @return {URL} url
+ *  The converted URL.
+ */
+function strToURL(str, hostname = true) {
+  let url = new URL(str);
+  if (hostname) {
+    if (url.hostname.includes('.')) {
+      url.hostname = url.hostname.replace(/^www\./, '');
+    }
+  }
+  return (url);
+}
