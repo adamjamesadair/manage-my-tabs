cb9169539ec1420f6098a8f4c4ab4d43903592ee
Adam Adair <adam.james.adair@gmail.com>
2018-05-30 13:56:41 +0900

    Fixed undo opening duplicate tabs (#102)

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  addListeners(new TabManager());
   tabManager = new TabManager();
   addListeners(tabManager);
   tabManager.reloadPage();
