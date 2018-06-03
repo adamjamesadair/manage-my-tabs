bc76c5bfa8f3b99478e12819e0aababccf3cacfa
adamjamesadair <adam.james.adair@gmail.com>
2018-06-04 08:28:08 +0900

    Initial commit

--- scripts/src/bg.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.windows.getLastFocused({populate: true}, window => {
-    chrome.tabs.create({url: 'tabPage.html', windowId: window.id});
-  });
-});
