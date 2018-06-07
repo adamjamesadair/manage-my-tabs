8560ed6f061b1abfaf05d737bd5aa0e58233211c
adamjamesadair <adam.james.adair@gmail.com>
2018-06-07 11:49:48 +0900

    Added border and reduced delay in window animation

--- scripts/src/listeners.js
+++ scripts/src/listeners.js
@@ -188,9 +188,8 @@ function addTabManagerListeners(tabManager) {
           });
           let $targetWindow = $('#windowWithTabGroups-' + tabManager.windows[btnID - 1].id);
           $targetWindow
-              .css('background-color', '')
               .addClass('selected-window', 250)
-              .delay(500)
+              .delay(350)
               .removeClass('selected-window', 500);
         }
       }
